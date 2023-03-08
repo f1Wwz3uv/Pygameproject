48ade08e624e7747b2db717ff2c4215d656c2828
Gustavo tavares Santoro Santos <e.gustavo.tss@gmail.com>
2023-03-08 18:16:50 -0300

    Jogo em python

---
+++ Pygame.py
@@ -0,0 +1,90 @@
+
+import pygame 
+
+from pygame.locals import *
+from sys import exit
+from random import randint
+
+
+pygame.init()
+
+pygame.mixer.music.set_volume(0.1)
+
+musica_de_fundo=pygame.mixer.music.load("BoxCat Games - CPU Talk.mp3")
+pygame.mixer.music.play(-1)
+
+barulho_colisao=pygame.mixer.Sound("smw_coin.wav")
+barulho_colisao.set_volume(0.7)
+
+largura=640
+altura=480
+x=largura//2
+y=altura//2
+
+x_azul= randint(40,600)
+y_azul=randint(50,430)
+
+pontos=0
+
+fonte=pygame.font.SysFont("Times new roman", 40,bold=True,italic=True)
+
+tela=pygame.display.set_mode((largura,altura))
+pygame.display.set_caption("Joguinho python")
+relogio=pygame.time.Clock()
+
+# Aqui definimos o nosso jogo
+
+# Ele deverá estar nesse loop while para que funcione.
+
+while True:
+
+    relogio.tick(80)
+    tela.fill((0,0,0))
+    mensagem=f'Pontos:{pontos}'
+    Texto_formatado=fonte.render(mensagem,True,(255,255,255))
+
+    for event in pygame.event.get():
+        if event.type== QUIT:
+            pygame.quit()
+            exit()
+
+        #if event.type==KEYDOWN:
+           # if event.key==K_a:
+            #    x-=20
+            #if event.key==K_d:
+             #   x+=20
+
+            #if event.key==K_w:
+             #   y-=20
+
+            #if event.key==K_s:
+              #  y+=20
+
+    if pygame.key.get_pressed()[K_a]:
+       x-=20
+
+    if pygame.key.get_pressed()[K_d]:
+       x+=20
+
+    if pygame.key.get_pressed()[K_w]:
+       y-=20
+
+    if pygame.key.get_pressed()[K_s]:
+       y+=20
+    
+
+    ret_vermelho=pygame.draw.rect(tela,(255,0,0),(x,y,40,50))
+    ret_azul=pygame.draw.rect(tela,(0,0,255),(x_azul,y_azul,40,50))
+    
+    if ret_vermelho.colliderect(ret_azul):
+       x_azul=randint(40,600)
+       y_azul=randint(50,430)
+       pontos+=1
+       barulho_colisao.play()
+
+    tela.blit(Texto_formatado,(450,40))
+
+    pygame.display.update()
+
+
+    
